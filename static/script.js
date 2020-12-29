7648e252d609094232785d404b6d16858fa52a2a
devansh-07 <sonidev0201@gmail.com>
2020-12-29 22:16:26 +0530

    Added a 404 Error Page and made some minor changes.

--- static/script.js
+++ static/script.js
@@ -157,3 +157,93 @@ function lightMode(){
 
 	localStorage.setItem('mode', 'light');
 }
+
+// 404 page section
+
+function show404()
+{
+	var bd = document.getElementsByTagName('body')[0];
+    bd.style.borderWidth = '0px';
+    document.getElementById("page").style.display = "block";
+    document.getElementById("prex").style.display = "none";
+    document.getElementById("prex").style.height = "0";
+    document.getElementById("prex").style.width = "0";
+    document.getElementById("preloader").style.display = "none";
+    document.getElementById("txtt").innerHTML = "";
+
+    var k = document.getElementById('switch');
+    k.addEventListener('change', check404Mode);
+
+    load404Mode();
+}
+
+function check404Mode(event){
+	if (event.target.checked)
+		dark404();
+	else
+		light404();
+}
+
+function load404Mode(){
+	if (localStorage.getItem('mode') == 'dark')
+		dark404();
+}
+
+
+function dark404(){
+	var body = document.getElementsByTagName('body')[0];
+	var horSep = document.getElementsByTagName('hr');
+	var nav = document.getElementById('NavBar');
+	var containers = document.getElementsByClassName('container');
+	var fluidContainers = document.getElementsByClassName('container-fluid');
+
+	nav.classList.replace("lightnav", "darknav");
+	nav.classList.replace("navbar-light", "navbar-dark");
+
+	body.style.backgroundColor = '#191919';
+	body.style.backgroundImage = "url('static/img/grid-dark.svg')";
+
+	for (var hr of horSep)
+		hr.classList.replace("bg-dark", "bg-light");
+
+	for (var cont of containers)
+		cont.classList.replace('text-dark', 'text-light')
+
+	for (var fcont of fluidContainers)
+		fcont.classList.replace('text-dark', 'text-light')
+	
+	if (! document.getElementById('switch').checked){
+		document.getElementById('switch').click();
+	}
+
+	localStorage.setItem('mode', 'dark');
+}
+
+function light404(){
+	var body = document.getElementsByTagName('body')[0];
+	var horSep = document.getElementsByTagName('hr');
+	var nav = document.getElementById('NavBar');
+	var containers = document.getElementsByClassName('container');
+	var fluidContainers = document.getElementsByClassName('container-fluid');
+
+	nav.classList.replace("darknav", "lightnav");
+	nav.classList.replace("navbar-dark", "navbar-light");
+
+	body.style.backgroundColor = '#f3f3f3';
+	body.style.backgroundImage = "url('static/img/grid-light.svg')";
+
+	for (var hr of horSep)
+		hr.classList.replace("bg-light", "bg-dark");
+
+	for (var cont of containers)
+		cont.classList.replace('text-light', 'text-dark')
+
+	for (var fcont of fluidContainers)
+		fcont.classList.replace('text-light', 'text-dark')
+
+	if (document.getElementById('switch').checked){
+		document.getElementById('switch').click();
+	}
+
+	localStorage.setItem('mode', 'light');
+}
